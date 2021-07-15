47db65a9863f62cea7d22bef61ffdd4748cc72a2
HectorLopez1998 <hectorlopezp1998@gmail.com>
2021-07-14 22:33:46 -0400

    commit tree d909c9bf48e34834e72a1466167300d48437efd9

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(string="roller-skate"){
+    return `This Saturday, I want to ${string}!`
+}
+
+function mondayWork(string="go to the office"){
+return `This Monday, I will ${string}.`
+}
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
